f8336da60207808a7b0c42800d0134206e9a8851
Karsten Dambekalns <karsten@typo3.org>
2013-10-31 16:53:40 +0100

    [TASK] Documentation tweaks

--- Classes/Flowpack/ElasticSearch/ContentRepositoryAdaptor/Command/NodeIndexCommandController.php
+++ Classes/Flowpack/ElasticSearch/ContentRepositoryAdaptor/Command/NodeIndexCommandController.php
@@ -53,8 +53,7 @@ class NodeIndexCommandController extends CommandController {
 	/**
 	 * (Re-)index all nodes
 	 *
-	 * This command indexes (or re-indexes) all nodes contained in the content repository. If the --drop-index flag is
-	 * set, any existing node index will be deleted before indexing.
+	 * This command indexes (or re-indexes) all nodes contained in the content repository.
 	 *
 	 * @param integer $limit Amount of nodes to index at maximum
 	 * @return void
