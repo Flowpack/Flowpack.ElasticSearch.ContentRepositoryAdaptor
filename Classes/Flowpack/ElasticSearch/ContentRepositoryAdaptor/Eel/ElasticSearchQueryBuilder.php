7508a25ca479edce47f2dea117688e16599ef45d
Jon Klixbüll Langeland <jon@klixbull.org>
2014-11-10 11:22:52 +0100

    [FEATURE] Highlighting support in fulltext search results
    Call ``.highlight()`` to wrap search term matches found in
    fulltext search results. Matches are wrapped in ``<em>`` tags.

--- Classes/Flowpack/ElasticSearch/ContentRepositoryAdaptor/Eel/ElasticSearchQueryBuilder.php
+++ Classes/Flowpack/ElasticSearch/ContentRepositoryAdaptor/Eel/ElasticSearchQueryBuilder.php
@@ -67,7 +67,7 @@ class ElasticSearchQueryBuilder implements QueryBuilderInterface, ProtectedConte
 	 *
 	 * @var array
 	 */
-	protected $unsupportedFieldsInCountRequest = array('fields', 'sort', 'from', 'size');
+	protected $unsupportedFieldsInCountRequest = array('fields', 'sort', 'from', 'size', '_source', 'highlight');
 
 	/**
 	 * The ElasticSearch request, as it is being built up.
@@ -459,12 +459,33 @@ class ElasticSearchQueryBuilder implements QueryBuilderInterface, ProtectedConte
 	 * @return QueryBuilderInterface
 	 */
 	public function fulltext($searchWord) {
-
 		$this->appendAtPath('query.filtered.query.bool.must', array(
 			'query_string' => array(
 				'query' => $searchWord
 			)
 		));
+		return $this;
+	}
+
+	/**
+	 * Wrap search term matches found in the result for highlighting purposes. Only available for fulltext searches.
+	 *
+	 * @param integer $fragmentSize
+	 * @param integer $fragmentCount
+	 * @return QueryBuilderInterface
+	 */
+	public function highlight($fragmentSize = 150, $fragmentCount = 2) {
+		$this->request['_source'] = array('__fulltext');
+		$this->request['highlight'] = array(
+			'fields' => array(
+				'text' => array(
+					'force_source' => TRUE,
+					'fragment_size' => $fragmentSize,
+					'no_match_size' => $fragmentSize,
+					'number_of_fragments' => $fragmentCount
+				)
+			)
+		);
 		return $this;
 	}
 
