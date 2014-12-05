7182a103b033658fbf6d70543663b3b252a1db96
Karsten Dambekalns <karsten@dambekalns.de>
2014-12-05 17:16:24 +0100

    [TASK] Use NodeInterface in one more place

--- Classes/Flowpack/ElasticSearch/ContentRepositoryAdaptor/Command/NodeIndexCommandController.php
+++ Classes/Flowpack/ElasticSearch/ContentRepositoryAdaptor/Command/NodeIndexCommandController.php
@@ -240,9 +240,9 @@ class NodeIndexCommandController extends CommandController {
 	}
 
 	/**
-	 * @param \TYPO3\TYPO3CR\Domain\Model\Node $currentNode
+	 * @param \TYPO3\TYPO3CR\Domain\Model\NodeInterface $currentNode
 	 */
-	protected function traverseNodes(\TYPO3\TYPO3CR\Domain\Model\Node $currentNode) {
+	protected function traverseNodes(\TYPO3\TYPO3CR\Domain\Model\NodeInterface $currentNode) {
 
 		if ($this->limit !== NULL && $this->indexedNodes > $this->limit) {
 			return;
