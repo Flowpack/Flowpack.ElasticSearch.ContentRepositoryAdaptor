d02fd2e5a747b2cf4478f7d464ebdcdc80b88e9f
Remus Lazar <remus@lazar.info>
2015-08-12 18:50:16 +0200

    FIX: simple indexing mode removed
    .. because it was broken.

--- Classes/Flowpack/ElasticSearch/ContentRepositoryAdaptor/Indexer/NodeIndexer.php
+++ Classes/Flowpack/ElasticSearch/ContentRepositoryAdaptor/Indexer/NodeIndexer.php
@@ -242,8 +242,6 @@ class NodeIndexer extends AbstractNodeIndexer
             }
 
             $this->updateFulltext($node, $fulltextIndexOfNode, $targetWorkspaceName);
-        } elseif ($this->isIndexingEnabled($node) === true) {
-            $document->store(); // simple indexing mode
         }
 
         $this->logger->log(sprintf('NodeIndexer: Added / updated node [%s] %s. ID: %s',
@@ -353,7 +351,7 @@ class NodeIndexer extends AbstractNodeIndexer
      */
     public function removeNode(NodeInterface $node)
     {
-        if (!$this->shouldIndexNode($node)) {
+        if ($this->isIndexingEnabled($node) === false) {
             return;
         }
 
