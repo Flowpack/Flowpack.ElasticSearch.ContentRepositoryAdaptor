c1d79c0ac29a8926a97d70a3fa08394d8ba845b3
Christian Schwahn <c.schwahn@paints.de>
2017-06-01 17:23:18 +0200

    [CLEAN] removed unstructured configuration

--- Classes/Flowpack/ElasticSearch/ContentRepositoryAdaptor/Eel/ElasticSearchQueryBuilder.php
+++ Classes/Flowpack/ElasticSearch/ContentRepositoryAdaptor/Eel/ElasticSearchQueryBuilder.php
@@ -656,6 +656,8 @@ class ElasticSearchQueryBuilder implements QueryBuilderInterface, ProtectedConte
     {
         $request = $this->getRequest();
         $timeBefore = microtime(true);
+        $this->elasticSearchClient->setSuffix('en');
+        
         $response = $this->elasticSearchClient->getIndex()->request('GET', '/_search', [], json_encode($request));
         $timeAfterwards = microtime(true);
 
