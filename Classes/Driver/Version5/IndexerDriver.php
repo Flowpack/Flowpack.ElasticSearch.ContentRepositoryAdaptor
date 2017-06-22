35b8c8fba0e71ed32413fb1e211fb2a4d867b5c4
Karsten Dambekalns <karsten@dambekalns.de>
2017-06-22 20:16:39 +0200

    FEATURE: Add support for Elasticsearch v5
    A new `5.x` setting enables a driver that supports Elasticsearch v5.
    
    The documentation has been adjusted and hints on (potentially) needed
    changes to Elasticsearch node type mappings are documented.

---
+++ Classes/Driver/Version5/IndexerDriver.php
@@ -0,0 +1,132 @@
+<?php
+namespace Flowpack\ElasticSearch\ContentRepositoryAdaptor\Driver\Version5;
+
+/*
+ * This file is part of the Flowpack.ElasticSearch.ContentRepositoryAdaptor package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Driver\AbstractNodeTypeMappingBuilder;
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Driver\Version2;
+use Flowpack\ElasticSearch\Domain\Model\Document as ElasticSearchDocument;
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\TYPO3CR\Domain\Model\NodeInterface;
+
+/**
+ * Indexer driver for Elasticsearch version 5.x
+ *
+ * @Flow\Scope("singleton")
+ */
+class IndexerDriver extends Version2\IndexerDriver
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function document($indexName, NodeInterface $node, ElasticSearchDocument $document, array $documentData)
+    {
+        if ($this->isFulltextRoot($node)) {
+            // for fulltext root documents, we need to preserve the "__fulltext" field. That's why we use the
+            // "update" API instead of the "index" API, with a custom script internally; as we
+            // shall not delete the "__fulltext" part of the document if it has any.
+            return [
+                [
+                    'update' => [
+                        '_type' => $document->getType()->getName(),
+                        '_id' => $document->getId(),
+                        '_index' => $indexName,
+                        '_retry_on_conflict' => 3
+                    ]
+                ],
+                // http://www.elasticsearch.org/guide/en/elasticsearch/reference/2.4/docs-update.html
+                [
+                    'script' => [
+                        'stored' => 'updateFulltextParts',
+                        'params' => [
+                            'newData' => $documentData
+                        ]
+                    ],
+                    'upsert' => $documentData
+                ]
+            ];
+        }
+
+        // non-fulltext-root documents can be indexed as-they-are
+        return [
+            [
+                'index' => [
+                    '_type' => $document->getType()->getName(),
+                    '_id' => $document->getId()
+                ]
+            ],
+            $documentData
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fulltext(NodeInterface $node, array $fulltextIndexOfNode, $targetWorkspaceName = null)
+    {
+        $closestFulltextNode = $node;
+        while (!$this->isFulltextRoot($closestFulltextNode)) {
+            $closestFulltextNode = $closestFulltextNode->getParent();
+            if ($closestFulltextNode === null) {
+                // root of hierarchy, no fulltext root found anymore, abort silently...
+                $this->logger->log(sprintf('NodeIndexer: No fulltext root found for node %s (%)', $node->getPath(), $node->getIdentifier()), LOG_WARNING, null, 'ElasticSearch (CR)');
+
+                return null;
+            }
+        }
+
+        $closestFulltextNodeContextPath = $closestFulltextNode->getContextPath();
+        if ($targetWorkspaceName !== null) {
+            $closestFulltextNodeContextPath = str_replace($node->getContext()->getWorkspace()->getName(), $targetWorkspaceName, $closestFulltextNodeContextPath);
+        }
+        $closestFulltextNodeDocumentIdentifier = sha1($closestFulltextNodeContextPath);
+
+        if ($closestFulltextNode->isRemoved()) {
+            // fulltext root is removed, abort silently...
+            $this->logger->log(sprintf('NodeIndexer (%s): Fulltext root found for %s (%s) not updated, it is removed', $closestFulltextNodeDocumentIdentifier, $node->getPath(), $node->getIdentifier()), LOG_DEBUG, null, 'ElasticSearch (CR)');
+
+            return null;
+        }
+
+        $this->logger->log(sprintf('NodeIndexer (%s): Updated fulltext index for %s (%s)', $closestFulltextNodeDocumentIdentifier, $closestFulltextNodeContextPath, $closestFulltextNode->getIdentifier()), LOG_DEBUG, null, 'ElasticSearch (CR)');
+
+        $upsertFulltextParts = [];
+        if (!empty($fulltextIndexOfNode)) {
+            $upsertFulltextParts[$node->getIdentifier()] = $fulltextIndexOfNode;
+        }
+
+        return [
+            [
+                'update' => [
+                    '_type' => AbstractNodeTypeMappingBuilder::convertNodeTypeNameToMappingName($closestFulltextNode->getNodeType()->getName()),
+                    '_id' => $closestFulltextNodeDocumentIdentifier
+                ]
+            ],
+            // http://www.elasticsearch.org/guide/en/elasticsearch/reference/current/docs-update.html
+            [
+                // first, update the __fulltextParts, then re-generate the __fulltext from all __fulltextParts
+                'script' => [
+                    'stored' => 'regenerateFulltext',
+                    'params' => [
+                        'identifier' => $node->getIdentifier(),
+                        'nodeIsRemoved' => $node->isRemoved(),
+                        'nodeIsHidden' => $node->isHidden(),
+                        'fulltext' => $fulltextIndexOfNode
+                    ],
+                ],
+                'upsert' => [
+                    '__fulltext' => $fulltextIndexOfNode,
+                    '__fulltextParts' => $upsertFulltextParts
+                ]
+            ]
+        ];
+    }
+}
---
+++ Classes/Driver/Version5/Mapping/NodeTypeMappingBuilder.php
@@ -0,0 +1,208 @@
+<?php
+namespace Flowpack\ElasticSearch\ContentRepositoryAdaptor\Driver\Version5\Mapping;
+
+/*
+ * This file is part of the Flowpack.ElasticSearch.ContentRepositoryAdaptor package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Driver\Version2;
+use Flowpack\ElasticSearch\Domain\Model\Index;
+use Flowpack\ElasticSearch\Domain\Model\Mapping;
+use Flowpack\ElasticSearch\Mapping\MappingCollection;
+use TYPO3\Flow\Annotations as Flow;
+use TYPO3\Flow\Error\Result;
+use TYPO3\Flow\Error\Warning;
+use TYPO3\Flow\Object\ObjectManagerInterface;
+use TYPO3\TYPO3CR\Domain\Model\NodeType;
+
+/**
+ * NodeTypeMappingBuilder for Elasticsearch version 2.x
+ *
+ * @Flow\Scope("singleton")
+ */
+class NodeTypeMappingBuilder extends Version2\Mapping\NodeTypeMappingBuilder
+{
+    /**
+     * Called by the Flow object framework after creating the object and resolving all dependencies.
+     *
+     * @param integer $cause Creation cause
+     */
+    public function initializeObject($cause)
+    {
+        parent::initializeObject($cause);
+        if ($cause === ObjectManagerInterface::INITIALIZATIONCAUSE_CREATED) {
+            $this->adjustStringTypeMapping($this->defaultConfigurationPerType);
+        }
+    }
+
+    /**
+     * Builds a Mapping Collection from the configured node types
+     *
+     * @param Index $index
+     * @return MappingCollection<\Flowpack\ElasticSearch\Domain\Model\Mapping>
+     */
+    public function buildMappingInformation(Index $index)
+    {
+        $this->lastMappingErrors = new Result();
+
+        $mappings = new MappingCollection(MappingCollection::TYPE_ENTITY);
+
+        $this->setupStoredScripts($index);
+
+        /** @var NodeType $nodeType */
+        foreach ($this->nodeTypeManager->getNodeTypes() as $nodeTypeName => $nodeType) {
+            if ($nodeTypeName === 'unstructured' || $nodeType->isAbstract()) {
+                continue;
+            }
+
+            $type = $index->findType(self::convertNodeTypeNameToMappingName($nodeTypeName));
+            $mapping = new Mapping($type);
+            $fullConfiguration = $nodeType->getFullConfiguration();
+            if (isset($fullConfiguration['search']['elasticSearchMapping'])) {
+                $fullMapping = $fullConfiguration['search']['elasticSearchMapping'];
+                $this->adjustStringTypeMapping($fullMapping);
+                $mapping->setFullMapping($fullMapping);
+            }
+
+            // https://www.elastic.co/guide/en/elasticsearch/reference/5.4/dynamic-templates.html
+            $mapping->addDynamicTemplate('dimensions', [
+                'path_match' => '__dimensionCombinations.*',
+                'match_mapping_type' => 'string',
+                'mapping' => [
+                    'type' => 'keyword'
+                ]
+            ]);
+            $mapping->setPropertyByPath('__dimensionCombinationHash', [
+                'type' => 'keyword'
+            ]);
+
+            foreach ($nodeType->getProperties() as $propertyName => $propertyConfiguration) {
+                if (isset($propertyConfiguration['search']) && isset($propertyConfiguration['search']['elasticSearchMapping'])) {
+                    if (is_array($propertyConfiguration['search']['elasticSearchMapping'])) {
+                        $propertyMapping = $propertyConfiguration['search']['elasticSearchMapping'];
+                        $this->adjustStringTypeMapping($propertyMapping);
+                        $mapping->setPropertyByPath($propertyName, $propertyMapping);
+                    }
+                } elseif (isset($propertyConfiguration['type']) && isset($this->defaultConfigurationPerType[$propertyConfiguration['type']]['elasticSearchMapping'])) {
+                    if (is_array($this->defaultConfigurationPerType[$propertyConfiguration['type']]['elasticSearchMapping'])) {
+                        $mapping->setPropertyByPath($propertyName, $this->defaultConfigurationPerType[$propertyConfiguration['type']]['elasticSearchMapping']);
+                    }
+                } else {
+                    $this->lastMappingErrors->addWarning(new Warning('Node Type "' . $nodeTypeName . '" - property "' . $propertyName . '": No ElasticSearch Mapping found.'));
+                }
+            }
+
+            $mappings->add($mapping);
+        }
+
+        return $mappings;
+    }
+
+    /**
+     * Store scripts used during indexing in Elasticsearch.
+     *
+     * @param Index $index
+     * @return void
+     */
+    protected function setupStoredScripts(Index $index)
+    {
+        $this->client->request(
+            'POST',
+            '/_scripts/updateFulltextParts',
+            [],
+            json_encode(['script' => [
+                'lang' => 'painless',
+                'code' => '
+                    HashMap fulltext = (ctx._source.containsKey("__fulltext") ? ctx._source.__fulltext : new HashMap());
+                    HashMap fulltextParts = (ctx._source.containsKey("__fulltextParts") ? ctx._source.__fulltextParts : new HashMap());
+                    ctx._source = params.newData;
+                    ctx._source.__fulltext = fulltext;
+                    ctx._source.__fulltextParts = fulltextParts'
+                ]
+            ])
+        );
+
+        $this->client->request(
+            'POST',
+            '/_scripts/regenerateFulltext',
+            [],
+            json_encode(['script' => [
+                'lang' => 'painless',
+                'code' => '
+                        ctx._source.__fulltext = new HashMap();
+                        if (!ctx._source.containsKey("__fulltextParts")) {
+                            ctx._source.__fulltextParts = new HashMap();
+                        }
+                        
+                        if (params.nodeIsRemoved || params.nodeIsHidden || params.fulltext.size() == 0) {
+                            if (ctx._source.__fulltextParts.containsKey(params.identifier)) {
+                                ctx._source.__fulltextParts.remove(params.identifier);
+                            }
+                        } else {
+                            ctx._source.__fulltextParts.put(params.identifier, params.fulltext);
+                        }
+    
+                        ctx._source.__fulltextParts.each {
+                            originNodeIdentifier, partContent -> partContent.each {
+                                bucketKey, content ->
+                                    if (ctx._source.__fulltext.containsKey(bucketKey)) {
+                                        value = ctx._source.__fulltext[bucketKey] + " " + content.trim();
+                                    } else {
+                                        value = content.trim();
+                                    }
+                                    ctx._source.__fulltext[bucketKey] = value;
+                            }
+                        }
+                        '
+                ]
+            ])
+        );
+    }
+
+    /**
+     * Adjust the mapping for string to text or keyword as needed.
+     *
+     * This is used to ease moving from ES 1.x and 2.x to 5.x by migrating the
+     * mapping like this:
+     *
+     * | 2.x                                       | 5.x                              |
+     * |-------------------------------------------|----------------------------------|
+     * | "type": "string", "index": "no"           | "type": "text", "index": false   |
+     * | "type": "string"[, "index": "analyzed"]   | "type": "text", "index": true    |
+     * | "type": "string", "index": "not_analyzed" | "type": "keyword", "index": true |
+     *
+     * @param array &$mapping
+     * @return void
+     */
+    protected function adjustStringTypeMapping(array &$mapping)
+    {
+        foreach ($mapping as &$item) {
+            if (!is_array($item)) {
+                continue;
+            }
+
+            if (isset($item['type']) && $item['type'] === 'string') {
+                if (isset($item['index']) && $item['index'] === 'not_analyzed') {
+                    $item['type'] = 'keyword';
+                    $item['index'] = true;
+                    unset($item['analyzer']);
+                } elseif (isset($item['index']) && $item['index'] === 'no') {
+                    $item['type'] = 'text';
+                    $item['index'] = false;
+                } else {
+                    $item['type'] = 'text';
+                    $item['index'] = true;
+                }
+            }
+
+            $this->adjustStringTypeMapping($item);
+        }
+    }
+
+}
