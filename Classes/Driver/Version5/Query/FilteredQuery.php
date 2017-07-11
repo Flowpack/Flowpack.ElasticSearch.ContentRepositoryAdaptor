9b9c671904b0a8b3c474199bd125b1841076fea1
Lars Lauger <lars.lauger@netlogix.de>
2017-07-11 15:14:53 +0200

    TASK: Implement ES5 FilteredQuery

--- Classes/Driver/Version5/Query/FilteredQuery.php
+++ Classes/Driver/Version5/Query/FilteredQuery.php
@@ -12,10 +12,37 @@ namespace Flowpack\ElasticSearch\ContentRepositoryAdaptor\Driver\Version5\Query;
  */
 
 use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Driver\Version1;
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Exception;
 
 /**
  * Default Filtered Query
  */
 class FilteredQuery extends Version1\Query\FilteredQuery
 {
+
+    /**
+     * {@inheritdoc}
+     */
+    public function fulltext($searchWord)
+    {
+        $this->appendAtPath('query.bool.must', [
+            'query_string' => [
+                'query' => $searchWord
+            ]
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function queryFilter($filterType, $filterOptions, $clauseType = 'must')
+    {
+        if (!in_array($clauseType, ['must', 'should', 'must_not'])) {
+            throw new Exception\QueryBuildingException('The given clause type "' . $clauseType . '" is not supported. Must be one of "must", "should", "must_not".',
+                1383716082);
+        }
+
+        $this->appendAtPath('query.bool.filter.bool.' . $clauseType, [$filterType => $filterOptions]);
+    }
+
 }
