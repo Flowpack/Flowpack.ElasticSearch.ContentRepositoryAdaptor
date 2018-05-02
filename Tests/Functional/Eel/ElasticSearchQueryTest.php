5f5be007d67acec8760c53bb03ed7aa3aa537344
Daniel Lienert <daniel@lienert.cc>
2018-05-02 22:57:43 +0200

    TASK: Add test for search and highlighting

--- Tests/Functional/Eel/ElasticSearchQueryTest.php
+++ Tests/Functional/Eel/ElasticSearchQueryTest.php
@@ -107,6 +107,45 @@ class ElasticSearchQueryTest extends FunctionalTestCase
     /**
      * @test
      */
+    public function fullTextSearchReturnsTheDocumentNode() {
+        /** @var ElasticSearchQueryResult $result */
+        $result = $this->getQueryBuilder()
+            ->fulltext('circum*')
+            ->execute();
+
+        $this->assertEquals(1, $result->count());
+
+        /** @var NodeInterface $node */
+        $node = $result->current();
+        $this->assertEquals('TYPO3.Neos.NodeTypes:Page', $node->getNodeType()->getName());
+        $this->assertEquals('test-node-1', $node->getName());
+    }
+
+
+    /**
+     * @test
+     */
+    public function fullTextHighlighting() {
+        $queryBuilder = $this->getQueryBuilder();
+
+        /** @var NodeInterface $resultNode */
+        $resultNode = $queryBuilder
+            ->fulltext('whistles')
+            ->log($this->getLogMessagePrefix(__METHOD__))
+            ->execute()
+            ->current();
+        $searchHitForNode = $queryBuilder->getFullElasticSearchHitForNode($resultNode);
+        $highlightedText = current($searchHitForNode['highlight']['__fulltext.text']);
+
+        $expected = 'A Scout smiles and <em>whistles</em> under all circumstances.';
+
+        $this->assertEquals($expected, $highlightedText);
+    }
+
+
+    /**
+     * @test
+     */
     public function elasticSearchQueryBuilderStartsClean()
     {
         /** @var ElasticSearchQueryBuilder $query */
