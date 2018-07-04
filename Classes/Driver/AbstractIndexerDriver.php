ed61ac562fa09361c7207d47b1e83d7bc7a87fdd
Daniel Lienert <daniel@lienert.cc>
2018-07-04 10:46:43 +0200

    TASK: Use findClosestFulltextRoot from abstract driver in ES5 driver

--- Classes/Driver/AbstractIndexerDriver.php
+++ Classes/Driver/AbstractIndexerDriver.php
@@ -48,7 +48,7 @@ abstract class AbstractIndexerDriver extends AbstractDriver
             if ($closestFulltextNode === null) {
                 // root of hierarchy, no fulltext root found anymore, abort silently...
                 if ($node->getPath() !== '/' && $node->getPath() !== '/sites') {
-                    $this->logger->log(sprintf('NodeIndexer: No fulltext root found for node %s (%s)', $node->getContextPath(), $node->getIdentifier()), LOG_WARNING, null, 'ElasticSearch (CR)');
+                    $this->logger->log(sprintf('NodeIndexer: No fulltext root found for node %s (%s)', $node->getIdentifier(), $node->getContextPath()), LOG_WARNING, null, 'ElasticSearch (CR)');
                 }
 
                 return null;
--- Classes/Driver/Version5/IndexerDriver.php
+++ Classes/Driver/Version5/IndexerDriver.php
@@ -77,15 +77,9 @@ class IndexerDriver extends Version2\IndexerDriver
      */
     public function fulltext(NodeInterface $node, array $fulltextIndexOfNode, $targetWorkspaceName = null)
     {
-        $closestFulltextNode = $node;
-        while (!$this->isFulltextRoot($closestFulltextNode)) {
-            $closestFulltextNode = $closestFulltextNode->getParent();
-            if ($closestFulltextNode === null) {
-                // root of hierarchy, no fulltext root found anymore, abort silently...
-                $this->logger->log(sprintf('NodeIndexer: No fulltext root found for node %s (%)', $node->getPath(), $node->getIdentifier()), LOG_WARNING, null, 'ElasticSearch (CR)');
-
-                return null;
-            }
+        $closestFulltextNode = $this->findClosestFulltextRoot($node);
+        if ($closestFulltextNode === null) {
+            return null;
         }
 
         $closestFulltextNodeContextPath = $closestFulltextNode->getContextPath();
