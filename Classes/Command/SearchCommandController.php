5594045e93cf592ae570c4bfa79b494c40551de4
Dominique Feyer <dfeyer@ttree.ch>
2019-04-08 11:17:35 +0200

    TASK: Add a CLI command to test fulltext search

---
+++ Classes/Command/SearchCommandController.php
@@ -0,0 +1,99 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Flowpack\ElasticSearch\ContentRepositoryAdaptor\Command;
+
+/*
+ * This file is part of the Flowpack.ElasticSearch.ContentRepositoryAdaptor package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Driver\NodeTypeMappingBuilderInterface;
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Eel\ElasticSearchQueryBuilder;
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Eel\ElasticSearchQueryResult;
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Eel\SearchResultHelper;
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\ElasticSearchClient;
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Indexer\NodeIndexer;
+use Flowpack\ElasticSearch\ContentRepositoryAdaptor\Service\DimensionsService;
+use Flowpack\ElasticSearch\Domain\Model\Mapping;
+use Neos\ContentRepository\Domain\Model\NodeInterface;
+use Neos\ContentRepository\Domain\Service\ContentDimensionCombinator;
+use Neos\ContentRepository\Domain\Service\ContextFactory;
+use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Cli\CommandController;
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * @Flow\Scope("singleton")
+ */
+class SearchCommandController extends CommandController
+{
+    /**
+     * @var ContextFactory
+     * @Flow\Inject
+     */
+    protected $contextFactory;
+
+    /**
+     * @Flow\Inject
+     * @var ElasticSearchClient
+     */
+    protected $elasticSearchClient;
+
+    /**
+     * @param string $path
+     * @param string $query
+     * @param string|null $dimensions
+     */
+    public function fulltextCommand(string $path, string $query, ?string $dimensions = null)
+    {
+        $contextConfiguration = [
+            'workspaceName' => 'live',
+        ];
+        if ($dimensions !== null) {
+            $contextConfiguration['dimensions'] = json_decode($dimensions, true);
+        }
+        
+        $context = $this->contextFactory->create($contextConfiguration);
+        $contextNode = $context->getNode($path);
+
+        if ($contextNode === null) {
+            $this->outputLine('Context node not found');
+            $this->sendAndExit(1);
+        }
+
+        $q = new ElasticSearchQueryBuilder();
+        $q = $q->query($contextNode)
+            ->log(__CLASS__)
+            ->fulltext($query)
+            ->limit(100)
+            ->termSuggestions($query);
+
+        /** @var ElasticSearchQueryResult $results */
+        $results = $q->execute();
+
+        $didYouMean = (new SearchResultHelper())->didYouMean($results);
+        if (trim($didYouMean) !== '') {
+            $this->outputLine('Did you mean <comment>%s</comment>', [$didYouMean]);
+        }
+
+        $this->outputLine();
+        $this->outputLine('<info>Results</info>');
+        $this->outputLine('Number of result(s): %d', [$q->count()]);
+        $this->outputLine('Index name: %s', [$this->elasticSearchClient->getIndexName()]);
+        $this->outputLine();
+
+        /** @var NodeInterface $node */
+        foreach ($results as $node) {
+            $hits = $results->searchHitForNode($node);
+            $this->outputLine('- <b>%s</b> (%s)', [$node->getLabel(), $node->getNodeType()]);
+        }
+
+    }
+}
